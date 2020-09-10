a5b0c6575991a0c146464eba8225784331c4ec9b
Fraser Elliot Carter Smith <fraser.carter@gmail.com>
2020-09-10 16:46:47 +1000

    formatted terraform

--- main.tf
+++ main.tf
@@ -15,8 +15,8 @@
  */
 
 locals {
-  export_filter   = var.export_filter != "" ? var.export_filter : data.external.compute_filter.result.filter
-  machine_project = var.machine_project != "" ? var.machine_project : var.project_id
+  export_filter           = var.export_filter != "" ? var.export_filter : data.external.compute_filter.result.filter
+  machine_project         = var.machine_project != "" ? var.machine_project : var.project_id
   machine_network_project = var.machine_network_project == "" ? var.machine_project : var.machine_network_project
 }
 
